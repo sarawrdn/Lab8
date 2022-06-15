6137a1c0ccdb0ceb502a1ecd86b7129144cf923f
sara <sarawardina12@gmail.com>
2022-06-16 00:32:56 +0800

    just aa

---
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lab8;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.Result;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Lab8);
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v("TAG", rawResult.getText()); // Prints scan results
+        Log.v("TAG", rawResult.getBarcodeFormat().toString());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+        Toast.makeText(getApplicationContext(),"Successfully Scanned",Toast.LENGTH_SHORT).show();
+        mScannerView.resumeCameraPreview(this);
+    }
+    public void sendResult(Result rawResult) {
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+}
---
+++ app/src/main/java/com/example/lab8/SplashScreen.java
@@ -0,0 +1,12 @@
+package com.example.lab8;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));}
+}
